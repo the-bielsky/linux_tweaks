4a953ae859820ac39ee77ddba4f854c06f012182
PGr <przemyslaw.grzyska@solutinsca.pl>
2024-10-22 09:16:59 +0200

    - add git status to PS1 - setup improved.

--- sys_files/etc/bash_pgr_colors.sh
+++ sys_files/etc/bash_pgr_colors.sh
@@ -52,7 +52,7 @@ function git_status {
   if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
     echo -e "${fg_red}"  # Red color
   else
-    echo -e "$fg_green"  # Green color
+    echo -e "${fg_amber}"  # Green color
   fi
 }
 
@@ -75,50 +75,21 @@ function is_git_repo {
 # upper-left corner"
 # ┌─[root@przemekg]─[linux_tweaks]
 # └── # 
-# PS1="${fg_red}┌─[${username_color}\u${fg_red}${hostname_color}@\h${fg_red}]─[${fg_amber}\W${fg_red}]"
-# PS1="${PS1}\n"
-# PS1=${PS1}"${fg_red}└── ${fg_amber}\$${fg_off} "
 
-# PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]]\342\224\200\")"
-# # user data
-# PS1="[$(
-# if [[ ${EUID} == 0 ]];
-#     then echo '\[\033[01;31m\]root'${fg_red}@${username_color}'\h'; 
-#     else echo '\[\033[0;39m\]\u'${fg_amber}@${hostname_color}'\h'; 
-# fi
-
-# )\[\033[0;31m\]]"
-
-# \$( [ -n \"${NETNS}\" ]  &&  echo \"\342\224\200[\[${ns_colour}\]netns \${NETNS}\[${fg_red}\]]\"    )\
-# \$( [ -n \"\${tag}\" ]  &&  echo \"\342\224\200[\[${fg_amber}tag \${tag}\[${fg_red}\]]\"    )\
-# \$( [ -n \"\${build}\" ]  &&  echo \"\342\224\200[\[${fg_amber}build \${build}\[${fg_red}\]]\"    )\
-# \342\224\200[${fg_amber}\W\[\033[0;31m\]]\n\[\033[0;31m\]\342\224\224\342\224\200\
-# \$( [ -n \"\${PGR_DATESTAMP}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]\$(date +\%Y-\%m-\%d-\%H:\%M:\%S)${fg_red}]\")\
-# \$( [ -n \"\${PPJ1_CLIGRP}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]voipgrp \${PPJ1_CLIGRP}\[${fg_red}\]]\"    )\
-# \$( [ -n \"\${ipaddr}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]ipaddr \${ipaddr}\[${fg_red}\]]\"    )\
-# \$( [ \"\$(is_vpn_connection)\" -eq 1 ]  &&  echo \"\342\224\200[\[${fg_red}\]VPN]\"    )\
-# \342\224\200 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\] "
-# else
-#     PS1='─[\u@\h]─[\W]\n└── \$ '
-# fi
-
-
-# ─${fg_red}($(git_status)$(git_branch)${fg_red}/$(git_status)$(git_describe)${fg_red})\
-PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]]\342\224\200\")\
+# \$(git_status) runs every usage; $(git_status) runs once 
+PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]${fg_red}]\342\224\200\")\
 [$(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]root'${fg_red}@${hostname_color}'\h'; else echo '\[\033[0;39m\]\u'${fg_amber}@${hostname_color}'\h'; fi)\
 \[\033[0;31m\]]\
 \$( [ -n \"${NETNS}\" ]  &&  echo \"\342\224\200[\[${ns_colour}\]netns \${NETNS}\[${fg_red}\]]\"    )\
 \$( [ -n \"\${tag}\" ]  &&  echo \"\342\224\200[\[${fg_amber}tag \${tag}\[${fg_red}\]]\"    )\
 \$( [ -n \"\${build}\" ]  &&  echo \"\342\224\200[\[${fg_amber}build \${build}\[${fg_red}\]]\"    )\
-\342\224\200[${fg_amber}\w\[\033[0;31m\]]\n\[\033[0;31m\]\342\224\224\342\224\200\
+\342\224\200[${fg_amber}\w\[\033[0;31m\]]\n\[\033[0;31m\]\342\224\224\
 \$( [ -n \"\${PGR_DATESTAMP}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]\$(date +\%Y-\%m-\%d-\%H:\%M:\%S)${fg_red}]\")\
 \$( [ -n \"\${PPJ1_CLIGRP}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]voipgrp \${PPJ1_CLIGRP}\[${fg_red}\]]\"    )\
 \$( [ -n \"\${ipaddr}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]ipaddr \${ipaddr}\[${fg_red}\]]\"    )\
-\$( [ -n \"\${git_branch}\" ]  &&  echo \"\342\224\200[\[$(git_status)\]branch $(git_branch)\[${fg_red}\]]\"    )\
+\$( [ -n \"\$(git_branch)\" ]  &&  echo \"\342\224\200[\[\$(git_status)\]git: \$(git_branch)\]\[${fg_red}]\"    )\
 \$( [ \"\$(is_vpn_connection)\" -eq 1 ]  &&  echo \"\342\224\200[\[${fg_red}\]VPN]\"    )\
 \342\224\200 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\] "
-# else
-
 
 # Set 'man' colors
 man() {
