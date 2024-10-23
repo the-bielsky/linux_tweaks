bf604645d90eb576f5de9d4b0e42af89fd3916d8
PGr <przemyslaw.grzyska@solutinsca.pl>
2024-10-23 07:46:51 +0200

    original bashrc added to removeit/ dir; PS1 corrected;

--- setup.sh
+++ setup.sh
@@ -6,6 +6,21 @@ for user in `ls -1 /home/`; do
     su -c "rsync -ar sys_files/etc/skel/ /home/$user/" $user
 done
 
+
+
+
+# if [ "$color_prompt" = yes ]; then
+#     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+#     for COLORS_FILE in ~/.bash_pgr_colors.sh /etc/bash_pgr_colors.sh; do
+#         if [ -f ${COLORS_FILE} ]; then
+#             . ${COLORS_FILE}
+#             break
+#         fi
+#     done
+# else
+#     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+# fi
+
 # SRC=$PWD/sys_files/usr/local/bin/
 # ls $SRC
 # DST=/usr/local/bin/
--- sys_files/etc/bash_pgr_colors.sh
+++ sys_files/etc/bash_pgr_colors.sh
@@ -77,6 +77,7 @@ function is_git_repo {
 # └── # 
 
 # \$(git_status) runs every usage; $(git_status) runs once 
+# \$( [ -n \"\$(git_branch)\" ]  &&  echo \"\342\224\200[\[\$(git_status)\]git: \$(git_branch)\]\[${fg_red}]\"    )\
 PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]${fg_red}]\342\224\200\")\
 [$(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]root'${fg_red}@${hostname_color}'\h'; else echo '\[\033[0;39m\]\u'${fg_amber}@${hostname_color}'\h'; fi)\
 \[\033[0;31m\]]\
@@ -87,7 +88,7 @@ PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0
 \$( [ -n \"\${PGR_DATESTAMP}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]\$(date +\%Y-\%m-\%d-\%H:\%M:\%S)${fg_red}]\")\
 \$( [ -n \"\${PPJ1_CLIGRP}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]voipgrp \${PPJ1_CLIGRP}\[${fg_red}\]]\"    )\
 \$( [ -n \"\${ipaddr}\" ]  &&  echo \"\342\224\200[\[${fg_amber}\]ipaddr \${ipaddr}\[${fg_red}\]]\"    )\
-\$( [ -n \"\$(git_branch)\" ]  &&  echo \"\342\224\200[\[\$(git_status)\]git: \$(git_branch)\]\[${fg_red}]\"    )\
+\$( [ -n \"\$(git_branch)\" ]  &&  echo \"\342\224\200[\[\$(git_status)\]git: \$(git_branch)\]\[${fg_red}\]]\"    )\
 \$( [ \"\$(is_vpn_connection)\" -eq 1 ]  &&  echo \"\342\224\200[\[${fg_red}\]VPN]\"    )\
 \342\224\200 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\] "
 
