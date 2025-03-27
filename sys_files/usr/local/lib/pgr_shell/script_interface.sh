e11d4039950eba1d1cc2c566470dfed9055161be
PGr@workstation <pgr@sca>
2025-03-27 07:29:34 +0100

    - functions for generating script helps added

--- sys_files/usr/local/lib/pgr_shell/script_interface.sh
+++ sys_files/usr/local/lib/pgr_shell/script_interface.sh
@@ -9,6 +9,20 @@ GRAY='\033[1;30m'
 AMBER='\033[0;33m'
 NC='\033[0m' # No Color
 
+function pgr_get_next_line_after_function_definitions() {
+    # pgr_get_next_line_after_function_definitions <input_file> - get next line after function definitions
+    local input_file=$1
+    grep -nE "^\W*function\W+[a-zA-Z]" "$input_file" | while IFS=: read -r line_number _; do
+        next_line=$((line_number + 1))
+        sed -n "${next_line}p" "$input_file"
+    done
+}
+function pgr_get_help_lines() {
+    # pgr_get_help_lines <input_file> - get next line after function definitions that starts with #
+    local input_file="$1"
+    pgr_get_next_line_after_function_definitions "${input_file}" | grep -E "^\W*#"
+}
+
 shout () {
     echo -e "$RED$*$NC" >&2
 }
