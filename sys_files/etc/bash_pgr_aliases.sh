f45152815131112820fa7741fa15db99818cd6a7
PGr@workstation <pgr@sca>
2025-03-31 10:27:01 +0200

    - ifixed some problems with setup

--- sys_files/etc/bash_pgr_aliases.sh
+++ sys_files/etc/bash_pgr_aliases.sh
@@ -23,16 +23,13 @@ alias shcon='source /usr/local/bin/sshconnect.sh'
 function _git_pullpush_(){
     operation=$1
     BRANCH=$(git branch --show-current); 
-    echo wtf
     if $(git_status_numeric); then
         echo GIT: $operation origin $BRANCH; 
-        echo ??
         sleep 1; 
         trap "echo operation failed; return 1" ERR
         git $operation origin ${BRANCH}
-        echo $operation
         git $operation --tags
-        echo $?
+        echo "pulling finished"
         return 0
     else
         echo Branch $BRANCH is not commited\; operation aborted
