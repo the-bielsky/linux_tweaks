7e4b0a9b39907c43151ba747ad3babacd5e905ee
PGr@workstation <pgr@sca>
2025-07-09 09:01:25 +0200

    github backup updated

--- sys_files/usr/local/bin/pgr_github_backup.sh
+++ sys_files/usr/local/bin/pgr_github_backup.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-# This script is used to backup the github repositories to the local machine.
-# The script will clone the repositories to the local machine
-# requires installed gh (github cli) and jq (json parser)
-# gh cli must be authorized with syntax:
-# gh auth login
-
-# $1 must be provided as the first argument
-
 help(){
-    echo
-    echo "Usage: $0 [backup_dir]"
-    echo
-    echo "Backup all repositories of the owner to the backup_dir"
-    echo
-    echo "backup_dir:    directory where the repositories will be cloned"
-    echo "If backup_dir is not provided, use env variable PGR_GIT_BACKUP_DIR"
-    echo "Then if PGR_GIT_BACKUP_DIR is not set, use current directory"
-    echo
-    echo "File github_backup.conf must exists in backup_dir."
-    echo "This file may be empty or contain the variable PGR_REPO_OWNER"
-    echo "Repo owner is the github user or organization"
-    echo "Variable PGR_REPO_OWNER may also be provided as exported shell variable"
-    echo "Variable has higher priority than config value, but config file must exist"
-    echo "Variable may contain multiple owners separated by space, e.g.:"
-    echo "          PGR_REPO_OWNER=(repo_owner1 repo_owner2)"
-    echo
-    echo "The script requires installed gh (github cli) and jq (json parser)"
-    echo "gh cli must be authorized with syntax:"
-    echo "gh auth login"
-    echo
+    echo "
+Usage: $0 [backup_dir]
+
+Backup all repositories of the owner to the backup_dir
+
+Arguments:
+    backup_dir:    directory where the repositories will be cloned
+                   If backup_dir is not provided, use env variable PGR_GIT_BACKUP_DIR
+                   Then if PGR_GIT_BACKUP_DIR is not set, use current directory
+Config file:
+    github_backup.conf must exist in backup_dir.
+    This file may be empty or contain the variable PGR_REPO_OWNER
+    Repo owner is the github user or organization
+    Variable PGR_REPO_OWNER may also be provided as exported shell variable
+    Variable has higher priority than config value, but config file must exist
+    Variable may contain multiple owners separated by space, e.g.:
+         export PGR_REPO_OWNER=(repo_owner1 repo_owner2)
+
+REQUIREMENTS:
+    - gh (github cli) 
+    - jq (json parser)
+
+gh cli must be authorized with syntax:
+gh auth login
+"
 }
 
 PGR_GIT_BACKUP_DIR=${1:-$PGR_GIT_BACKUP_DIR}  # Directory where the repositories will be cloned
@@ -69,6 +65,7 @@ trap 'echo Error!; ERRCNT=$((ERRCNT+1)); exit 1' ERR
 # Healthcheck; statement in the loop doesnt generate error if gh repo list has error
 res=$(gh repo list $PGR_REPO_OWNER)
 # echo $res
+# gh repo list solution-sca --json name,diskUsage --jq '.[] | "\(.name)"' | wc -l
 for owner in ${PGR_REPO_OWNER[@]}; do
     echo -------------------------------------------------------------------------------
     echo -e "Cloning repositories of $owner to $PGR_GIT_BACKUP_DIR"
