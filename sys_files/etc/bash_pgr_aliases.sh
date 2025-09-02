abdf9ca446d664b6514876f02f8021fa4f313334
PGr@workstation <pgr@sca>
2025-09-02 09:04:36 +0200

    setup updated 3

--- sys_files/etc/bash_pgr_aliases.sh
+++ sys_files/etc/bash_pgr_aliases.sh
@@ -7,3 +7,37 @@ alias l='ls -CF'
 alias la='ls -lha'
 alias ll='ls -lh'
 alias shcon='source /usr/local/bin/sshconnect.sh'
+
+alias apti='sudo -E apt -y install'
+alias aptu='sudo apt update && echo "------------" && apt list --upgradable && echo "------------" && sudo apt upgrade'
+alias datestamp='date +%Y%m%d-%H%M'
+# alias beeper='isok=$? /usr/local/sbin/beeper'
+# alias cho='sudo -E chown -R 1000:1000'
+# alias em='emacs -nw'
+# alias fuck='TF_CMD=$(TF_ALIAS=fuck PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1)) && eval $TF_CMD && history -s $TF_CMD'
+# alias fucking='sudo'
+# alias reboot='sl'
+# alias Reboot='systemctl reboot'
+alias please='sudo'
+alias stamp='if [ -n "${PGR_DATESTAMP}" ]; then unset PGR_DATESTAMP; else export PGR_DATESTAMP=1; fi'
+alias shcon='source /usr/local/bin/sshconnect.sh'
+
+function _git_pullpush_(){
+    operation=$1
+    BRANCH=$(git branch --show-current); 
+    echo GIT: $operation origin $BRANCH; 
+    if $(git_status_numeric); then
+        sleep 1; 
+        trap "echo operation failed; return 1" ERR
+        git $operation origin ${BRANCH}
+        return 0
+    else
+        echo Branch $BRANCH is not commited\; operation aborted
+        return 1
+    fi
+}
+
+alias gish='_git_pullpush_ push'
+alias gill='_git_pullpush_ pull'
+
+alias vin='code-insiders'
