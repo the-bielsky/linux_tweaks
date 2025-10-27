041da6f734082a7f66d1dffa1a61e38fbdd0056c
bielsky@home <bielsky@home>
2025-10-27 08:14:38 +0100

    scripts revision

--- sys_files/usr/local/lib/pgr_shell/script_helpers.sh
+++ sys_files/usr/local/lib/pgr_shell/script_helpers.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-function check_git_changes(){
-    # depracated module
-    changes=$(git status -s | wc -l)
-    if [ $changes -ne 0 ]; then
-        shout "WARNING: There are uncommited changes in the repository"
-        return 1
-    fi
-}
